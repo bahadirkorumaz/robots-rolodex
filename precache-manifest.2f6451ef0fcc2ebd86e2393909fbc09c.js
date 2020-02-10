38c99173404651ee9590e9f6cf097e769d348e69
Bahadır KORUMAZ <bkorumaz@havelsan.com.tr>
2020-02-10 15:33:31 +0300

    Updates

---
+++ precache-manifest.2f6451ef0fcc2ebd86e2393909fbc09c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a679a817e51d683d6c45982a749bd998",
+    "url": "/robots-rolodex/index.html"
+  },
+  {
+    "revision": "7d958e502df0afd48416",
+    "url": "/robots-rolodex/static/css/main.76b2cdb5.chunk.css"
+  },
+  {
+    "revision": "5839ed3c83c8fcce4b8e",
+    "url": "/robots-rolodex/static/js/2.7b328a78.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/robots-rolodex/static/js/2.7b328a78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d958e502df0afd48416",
+    "url": "/robots-rolodex/static/js/main.7b016085.chunk.js"
+  },
+  {
+    "revision": "2ad57ae96986df441f85",
+    "url": "/robots-rolodex/static/js/runtime-main.05f4a127.js"
+  }
+]);
